2189fe97f33623b79dec20203bbb9840532869d2
John Bartel <john@johnbartel.us>
2025-04-25 22:30:27 +0000

    Fix HMR in devcontainer by add polling option

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd())
+	const pollInterval = parsePollInterval(env.VITE_POLL);
+	const enablePolling = pollInterval > 0;
+
+	if (enablePolling) console.log("Polling every", pollInterval, "ms");
+
+	return {
+		plugins: [sveltekit()],
+		server: {
+			watch: {
+				usePolling: enablePolling,
+				interval: pollInterval,
+			}
+		}
+	}
+})
+// Parse the poll interval from the environment variable
+function parsePollInterval(pollInterval: string | undefined): number {
+	if (!pollInterval) {
+		return 0;
+	}
+	const parsed = parseFloat(pollInterval)
+	if (isNaN(parsed)) {
+		return 0
+	}
+	// Shortcut for values less than 10 to be in seconds
+	if (parsed < 10) {
+		return parsed * 1000;
+	}
+	return parsed
+}

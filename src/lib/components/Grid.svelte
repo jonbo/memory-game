7b8026bbb43fc3c8b7aafa2d9b63d9a34793beb1
John Bartel <john@johnbartel.us>
2025-05-01 21:01:37 +0000

    Refactor and simplify grid responsiveness using a more svelte approach

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -9,7 +9,8 @@
 		cellsData: CellState[];
 		onCellClick: (row: number, col: number) => void;
 	}>();
-	let grid = $state<HTMLElement | undefined>();
+	let gridEl = $state<HTMLElement | undefined>();
+	let zoomLevel: number = $state(1);
 
 	$effect(() => {
 		cols; // dependency
@@ -19,23 +20,15 @@
 	});
 	// Make the grid responsive by scaling it based on the available width
 	function scaleGrid() {
-		if (!grid) return;
+		if (!gridEl) return;
 
-		const availableWidth = document.body.offsetWidth - grid.offsetLeft * 2; // Account for grid margins/padding
-		const zoomLevel = availableWidth / grid.offsetWidth;
-
-		if (zoomLevel < 1) {
-			grid.style.setProperty('transform', `scale(${zoomLevel})`);
-			grid.style.setProperty('transform-origin', 'top left');
-		} else {
-			grid.style.removeProperty('transform');
-			grid.style.removeProperty('transform-origin');
-		}
+		const availableWidth = document.body.offsetWidth - gridEl.offsetLeft * 2; // Account for grid margins/padding
+		zoomLevel = availableWidth / gridEl.offsetWidth;
 	}
 </script>
 
 <svelte:window on:resize={scaleGrid} />
-<div bind:this={grid} class="grid" style:--cols={cols}>
+<div bind:this={gridEl} class="grid" style:--cols={cols} style:--zoomLevel={zoomLevel}>
 	{#each cellsData as cell, index (index)}
 		{@const row = Math.floor(index / cols)}
 		{@const col = index % cols}
@@ -59,5 +52,8 @@
 		margin-right: auto; /* Center grid */
 
 		grid-template-columns: repeat(var(--cols, 8), 1fr);
+
+		transform: scale(var(--zoomLevel, 1));
+		transform-origin: top left;
 	}
 </style>

4f12b058f4fc3dd64391935d2d268e4b1787f6a4
John Bartel <john@johnbartel.us>
2025-05-02 21:39:15 +0000

    Fix: the custom random number generator being too predictable in first few values

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,17 +1,28 @@
 export class SeededRandom {
-	seed: number;
+	#seed: number;
 
 	constructor(seed: number) {
-		this.seed = seed;
+		this.#seed = seed;
+		this.warmUp();
+	}
+	// Warm up the generator by discarding the first few values
+	warmUp() {
+		for (let i = 0; i < 10; i++) {
+			this.next();
+		}
+	}
+	set seed(value: number) {
+		this.#seed = value;
+		this.warmUp();
 	}
 
 	// xorshift algorithm for generating pseudo-random numbers
 	next(): number {
-		let result = this.seed;
+		let result = this.#seed;
 		result ^= result << 13;
 		result ^= result >> 17;
 		result ^= result << 5;
-		this.seed = result;
+		this.#seed = result;
 		return (result >>> 0) / 4294967296;
 	}
 
